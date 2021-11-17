5d66ff4b138c6e12e6a0cd72cde0835bb99493a1
tejaanuchuri <tejaanuchuri0009@gmail.com>
2021-11-18 01:24:34 +0530

    Add project files.

---
+++ WpfApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp
+{
+    public class Program
+    {
+        [System.STAThreadAttribute()]
+        public static void Main()
+        {
+            using (new MyUWPApp.App())
+            {
+                WpfApp.App app = new WpfApp.App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        }
+    }
+}
